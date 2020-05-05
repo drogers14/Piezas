5ca37bec82bf00b2487efc7d031153044b005c17
Destiny Rogers <destinyrogers725@gmail.com>
2020-05-05 00:19:12 -0700

    constructor called?

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -9,7 +9,9 @@ class PiezasTest : public ::testing::Test
 {
 	protected:
 		PiezasTest(){} //constructor runs before each test
-		virtual ~PiezasTest(){} //destructor cleans up after tests
+		virtual ~PiezasTest(){
+    Piezas();
+    } //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
